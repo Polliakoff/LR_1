56a3b98d20493a086a35728052d4e3b5390a5317
Polliakoff <70587587+Polliakoff@users.noreply.github.com>
2020-09-20 18:28:32 +0300

    Реализован ввод параметров трубы из консоли
    В файле main.cpp реализован ввод параметров в переменную пользовательского типа truba_type из консоли

--- LR_1/main.cpp
+++ LR_1/main.cpp
@@ -1,15 +1,79 @@
 #include <iostream>
 #include "truba_type.h"
 #include "KS_type.h"
+#include <string>
 using namespace std;
+
 
 int main() {
 
+	setlocale(LC_ALL, "Russian");
+
+	//временные переменные дл€ разных задач
+	int temp_int;
+	double temp_double;
+	string temp_string;
+	
+	//объ€вление переменных пользовательских типов
+	truba_type pipe_1;
+	KS_type KS_1;
+
+	//¬вод параметров трубы
+	cout << "¬ведите параметры дл€ “рубы:"<<endl;
+	
+	cout << "¬ведите id трубы"<<endl;
+	cin >> pipe_1.id;
+	cout<< endl;
+
+	cout << "¬ведите длину трубы" << endl;
+	cin >> pipe_1.length;
+	cout << endl;
+
+	cout << "¬ведите диаметр трубы" << endl;
+	cin >> pipe_1.diameter;
+	cout << endl;
+
+	cout << "¬ ремонте ли труба (yes/no)" << endl;
+	cin >> temp_string;
+	cout << endl;
+
+	while (true) {
+
+		if (temp_string == "yes") {
+			pipe_1.in_servise = 1;
+			break;
+		}
+		else if (temp_string == "no") {
+			pipe_1.in_servise = 0;
+			break;
+		}
+		else {
+			cout << "¬ведите 'yes' или 'no' (строчными буквами)";
+		}
+		cout << endl;
+		cin >> temp_string;
+	}
+
+	
+
+	
+
+	
 
 
 
 
 
 
+
+	
+
+
+
+
+
+	
+	
+	system("Pause");
 	return 0;
 }
